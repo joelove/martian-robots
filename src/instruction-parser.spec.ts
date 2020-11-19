324b302d7f6f59d77ec88c53e3a9a8fea5ea9599
Joe Love <git@joelove.uk>
2020-11-19 14:45:49 +0000

    make second test pass

--- src/instruction-parser.spec.ts
+++ src/instruction-parser.spec.ts
@@ -1,21 +1,18 @@
 import instructionParser, {
-  XCoord,
-  YCoord,
-  Direction,
-  Instruction,
   WorldSize,
-  Postition,
+  Position,
+  InstructionString,
 } from "./instruction-parser";
 
 describe("Instruction Parser", () => {
   let worldSize: WorldSize;
-  let startingPosition: Postition;
-  let instruction: Instruction;
+  let startingPosition: Position;
+  let instructionString: InstructionString;
 
   afterEach(() => {
     worldSize = [0, 0];
     startingPosition = [0, 0, "N"];
-    instruction = "";
+    instructionString = "";
   });
 
   describe("when the world size is 5 3", () => {
@@ -26,26 +23,26 @@ describe("Instruction Parser", () => {
     describe("when the starting position is 1 1 E and the instruction is RFRFRFRF", () => {
       beforeEach(() => {
         startingPosition = [1, 1, "E"];
-        instruction = "RFRFRFRF";
+        instructionString = "RFRFRFRF";
       });
 
       it("returns the expected output of 1 1 E", () => {
         expect(
-          instructionParser(worldSize, startingPosition, instruction)
+          instructionParser(worldSize, startingPosition, instructionString)
         ).toEqual([1, 1, "E"]);
       });
     });
 
     describe("when the starting position is 3 2 N and the instruction is FRRFLLFFRRFLL", () => {
       beforeEach(() => {
-        startingPosition = [1, 1, "E"];
-        instruction = "FRRFLLFFRRFLL";
+        startingPosition = [3, 2, "E"];
+        instructionString = "FRRFLLFFRRFLL";
       });
 
-      it("returns the expected output of 1 1 E", () => {
+      it("returns the expected output of 3 3 N", () => {
         expect(
-          instructionParser(worldSize, startingPosition, instruction)
-        ).toEqual([1, 1, "E"]);
+          instructionParser(worldSize, startingPosition, instructionString)
+        ).toEqual([3, 3, "N"]);
       });
     });
   });
--- src/instruction-parser.ts
+++ src/instruction-parser.ts
@@ -1,12 +1,58 @@
+const directions = ["N", "E", "S", "W"] as const;
+
+export type Direction = typeof directions[number];
+export type Instruction = "F" | "L" | "R";
+export type InstructionString = string;
 export type XCoord = number;
 export type YCoord = number;
-export type Instruction = string;
-export type Direction = "N" | "E" | "S" | "W";
+export type Coordinates = [XCoord, YCoord];
 export type WorldSize = [XCoord, YCoord];
-export type Postition = [XCoord, YCoord, Direction];
+export type Position = [XCoord, YCoord, Direction];
+export type ApplicatorFunction = (position: Position) => Position;
+
+const ForwardTransformations: Record<Direction, Coordinates> = {
+  N: [0, 1],
+  E: [1, 0],
+  S: [0, -1],
+  W: [-1, 0],
+};
+
+export const applyForward: ApplicatorFunction = ([x, y, d]) => {
+  const [transformX, transformY] = ForwardTransformations[d];
+
+  return [x + transformX, y + transformY, d];
+};
+
+export const applyLeft: ApplicatorFunction = ([x, y, d]) => {
+  const newDirection =
+    directions[directions.indexOf(d) - 1] ?? directions[directions.length - 1];
+
+  return [x, y, newDirection];
+};
+
+export const applyRight: ApplicatorFunction = ([x, y, d]) => {
+  const newDirection = directions[directions.indexOf(d) + 1] ?? directions[0];
+
+  return [x, y, newDirection];
+};
+
+export const InstructionApplicators: Record<Instruction, ApplicatorFunction> = {
+  F: applyForward,
+  L: applyLeft,
+  R: applyRight,
+} as const;
+
+const applyInstruction = (
+  currentPosition: Position,
+  instruction: Instruction
+): Position => InstructionApplicators[instruction](currentPosition);
 
 export default (
   worldsize: WorldSize,
-  startingPosition: Postition,
-  instruction: Instruction
-) => [1, 1, "E"];
+  startingPosition: Position,
+  instructionString: InstructionString
+): Position => {
+  let instructions = instructionString.split("") as Instruction[];
+
+  return instructions.reduce(applyInstruction, startingPosition);
+};
