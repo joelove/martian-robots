55a747c2bb571e000b44bfd2da587b21bacf744e
Joe Love <git@joelove.uk>
2020-11-19 13:45:07 +0000

    add second spec

--- src/instruction-parser.spec.ts
+++ src/instruction-parser.spec.ts
@@ -3,27 +3,50 @@ import instructionParser, {
   YCoord,
   Direction,
   Instruction,
+  WorldSize,
+  Postition,
 } from "./instruction-parser";
 
 describe("Instruction Parser", () => {
-  let worldSize: [XCoord, YCoord];
-  let startingPosition: [XCoord, YCoord, Direction];
+  let worldSize: WorldSize;
+  let startingPosition: Postition;
   let instruction: Instruction;
 
-  beforeEach(() => {
-    worldSize = [5, 3];
+  afterEach(() => {
+    worldSize = [0, 0];
+    startingPosition = [0, 0, "N"];
+    instruction = "";
   });
 
-  describe("when the starting position is 1 1 E and the instruction is RFRFRFRF", () => {
+  describe("when the world size is 5 3", () => {
     beforeEach(() => {
-      startingPosition = [1, 1, "E"];
-      instruction = "RFRFRFRF";
+      worldSize = [5, 3];
     });
 
-    it("returns the expected output of 1 1 E", () => {
-      expect(
-        instructionParser(worldSize, startingPosition, instruction)
-      ).toEqual([1, 1, "E"]);
+    describe("when the starting position is 1 1 E and the instruction is RFRFRFRF", () => {
+      beforeEach(() => {
+        startingPosition = [1, 1, "E"];
+        instruction = "RFRFRFRF";
+      });
+
+      it("returns the expected output of 1 1 E", () => {
+        expect(
+          instructionParser(worldSize, startingPosition, instruction)
+        ).toEqual([1, 1, "E"]);
+      });
+    });
+
+    describe("when the starting position is 3 2 N and the instruction is FRRFLLFFRRFLL", () => {
+      beforeEach(() => {
+        startingPosition = [1, 1, "E"];
+        instruction = "FRRFLLFFRRFLL";
+      });
+
+      it("returns the expected output of 1 1 E", () => {
+        expect(
+          instructionParser(worldSize, startingPosition, instruction)
+        ).toEqual([1, 1, "E"]);
+      });
     });
   });
 });
