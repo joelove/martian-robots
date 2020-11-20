ecc291ab6ef2e3f0b0f4f83b0215eda0297956ad
Joe Love <git@joelove.uk>
2020-11-20 11:48:28 +0000

    add first cli test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,22 @@
-import index from "./index";
+import {
+  mockProcessExit,
+  mockProcessStdout,
+  mockConsoleLog,
+} from "jest-mock-process";
 
-describe("Index", () => {
-  it("returns true", () => {
-    expect(index()).toEqual(true);
+import start from "./index";
+
+const stdoutMock = mockProcessStdout();
+
+// process.stdin.write("foo");
+
+describe("CLI", () => {
+  beforeEach(() => {
+    start();
   });
+
+  it("asks for the world size", async () =>
+    expect(stdoutMock).toHaveBeenCalledWith(
+      'Enter space-delimited world size (e.g. "3 5"): '
+    ));
 });
